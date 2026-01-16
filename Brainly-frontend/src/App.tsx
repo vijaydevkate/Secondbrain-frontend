addafeea7fed81ef95c9160b242fcfc9cf22b2e5
vijaydevkate <vijaydevkate98@gmail.com>
2026-01-16 10:30:00 -0500

    button components is rendering

--- Brainly-frontend/src/App.tsx
+++ Brainly-frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Button } from './components/ui/Button'
 
 function App() {
 
 
   return (
     <>
+    <Button variant='primary' text= "Share"></Button>
+    <Button variant='secondary' text="Add content"></Button>
       
     </>
   )
--- Brainly-frontend/src/components/ui/Button.tsx
+++ Brainly-frontend/src/components/ui/Button.tsx
@@ -1,17 +1,21 @@
 import type { ReactElement } from "react";
 interface Buttonprops {
     variant: "primary" | "secondary"
-    size: "sm" | "mg" | "lg"
+    size?: "sm" | "mg" | "lg"
     text: string
-    onClick: () => void;
+    onClick?: () => void;
     startIcon?: ReactElement;
     endIcon?: ReactElement;
 
 }
 
+//global button variable 
+const variantstyles = {
+    primary: "bg-purple-600 text-white",
+    secondary: "bg-purple-500 text-purple-600"
+}
 
-export const button = ( props: Buttonprops) => {
-    return <button></button>
-
+export const Button = ( props: Buttonprops) => {
+    return <button className={variantstyles[props.variant]}>{props.text}</button>
 }
 
